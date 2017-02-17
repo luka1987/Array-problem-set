8cb15a71e0a59d6963826c94e48b7c4db00c959e
luka1987 <zdravkovicx5b@gmail.com>
2017-02-17 22:37:54 +0100

    sumArray function works just fine. isUniform has some buggs

--- Array problem set.js
+++ Array problem set.js
@@ -1,5 +1,32 @@
+// FUNCTION REVERSE
+
 function reverse(arr){
-	for(var i = arr.length; i = 0; i--){
+	for(var i = arr.length; i >= 0; i--){
 		console.log(arr[i]);
 	}
+}
+
+// ISUNIFOM FUNCTION
+
+function isUniform(arr){
+	for(var i = 1; i < arr.length; i++){
+		if(arr[i] === arr[0]){
+			console.log(true);
+		}
+		else{
+			console.log(false);
+		}
+	}
+	
+}
+
+// SUMARRAY FUNCTION
+
+function sumArray(arr){
+	var result = 0;
+	for(var i = 0; i < arr.length; i++){
+		var index = arr[i];
+		result += index;
+	}
+	return result;
 }
